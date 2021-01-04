7506bc049759eb5b632b689cb948a74b45278116
sivan yeshayahu <sivanyesh@gmail.com>
2021-01-04 20:07:13 +0200

    change to new robot

--- Assets/Scripts/Robot.cs
+++ Assets/Scripts/Robot.cs
@@ -4,38 +4,20 @@ using UnityEngine;
 
 public class Robot : MonoBehaviour
 {
-
-
-    public Transform aimTarget; // the target where we aim to land the ball
     float speed;
     Rigidbody rb;
-    float f ;
-    // Start is called before the first frame update
-    // Start is called before the first frame update
-
-
-    float force; // ball impact force
-
-    bool hitting; // boolean to know if we are hitting the ball or not 
-
-    public Transform ball; // the ball 
-   
-
-    Vector3 aimTargetInitialPosition; // initial position of the aiming gameObject which is the center of the opposite court
-
-    
+    bool ball;
     GameObject robot;
     void Start()
     {
-        f = 100;
+       
         robot = GetComponent<GameObject>();
-        speed = 10f; //player speed 
+        speed = 7f; //player speed 
         rb = GetComponent<Rigidbody>();// an object creat and show in inspector help to control the player
-        
+        ball = false;
        
     }
 
-    
     // Update is called once per frame
     void Update()
     {
@@ -51,35 +33,22 @@ public class Robot : MonoBehaviour
         {
             transform.position += Vector3.right * speed * Time.deltaTime;//Current Location
         }
-        if (Input.GetKey(KeyCode.LeftArrow) )//player going left
+        if (Input.GetKey(KeyCode.LeftArrow))//player going left
         {
             transform.position -= Vector3.right * speed * Time.deltaTime;
-        }
 
-        /*
-        if (this.gameObject.CompareTag("Ball") && Input.GetKey(KeyCode.Space))
+        }
+    }
+    public void OnTriggerEnter(Collider other) {
+        if (other.gameObject.tag == "Ball" && Input.GetKey(KeyCode.UpArrow))
         {
-            //this.GetComponent<Rigidbody>().AddForce(Vector3.forward * 30.0f);//kick
+            other.GetComponent<Rigidbody>().AddForce(Vector3.forward * 100.0f);
         }
-        */
-
-
-
 
     }
 
 
-    /*
+    
 
-    void OnControllerColliderHit(ControllerColliderHit hit)
-    {
-        if (hit.gameObject.tag == "Ball")
-        {
-            Debug.Log("We hit a object");
-            hit.rigidbody.AddForce(transform.forward * f);
-        }
-    }
-    */
-
-
+    
 }
